ebcbba2082cf0adff6e1ef4b46da248bf1586f4a
enzo020 <enzosouza211106@gmail.com>
2026-08-04 19:28:00 -0300

    Criação da entidade Lead

---
+++ src/main/java/Jar/Dto/LeadRequestDTO.java
@@ -0,0 +1,4 @@
+package Jar.Dto;
+
+public record LeadRequestDTO() {
+}
---
+++ src/main/java/Jar/Repository/LeadRepository.java
@@ -0,0 +1,7 @@
+package Jar.Repository;
+
+import Jar.Model.Lead;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LeadRepository extends JpaRepository<Lead, Long> {
+}
